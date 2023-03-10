e39d272697d7d16f06eb823fa3ca6d2a3f07de5b
dimitr36 <dimitr36@msu.edu>
2023-03-09 19:25:29 -0500

    Delete comment from settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,8 +18,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
